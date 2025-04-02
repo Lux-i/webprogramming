b32fb06a2ac0bd513bf03f66c5b92a46bd822f14
LuxorUWU <s-s@s.com>
2025-04-02 14:38:22 +0200

    finished questionModule
    adding node_modules folder bcs too lazy

--- Lecture06/Exercise/templateExercise6/src/modules/questionModule.ts
+++ Lecture06/Exercise/templateExercise6/src/modules/questionModule.ts
@@ -8,6 +8,7 @@ export interface Question {
 
 export class QuestionModule {
   private questions: Array<Question> = [];
+  private _length = 0;
   //#region initial fetch-state info
   private isLoading = true;
   private isSuccess = false;
@@ -41,6 +42,7 @@ export class QuestionModule {
           "Retrieved empty array. Questions need to be stored first"
         );
       this.questions = data;
+      this._length = this.questions.length;
       return this.fetchSuccess();
     } catch (err) {
       console.log(err);
@@ -48,7 +50,18 @@ export class QuestionModule {
   }
   //#endregion
 
+  reset() {
+    this._length = this.questions.length;
+  }
+
   getQuestion(): Question {
-    return this.questions[Math.random()];
+    const index = Math.round(Math.random() * this._length - 1);
+    const selected = this.questions[index];
+    if (index < this._length - 1) {
+      this.questions[index] = this.questions[this._length - 1];
+      this.questions[this._length - 1] = selected;
+    }
+    this._length--;
+    return selected;
   }
 }
